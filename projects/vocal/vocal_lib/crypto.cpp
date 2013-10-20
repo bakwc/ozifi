65ff48257c9cfdac0e888266f02f4034c34572c0
Filipp Ozinov <ozinov@quantion.ru>
2013-10-20 15:46:50 +0400

    Added crypto interface

---
+++ projects/vocal/vocal_lib/crypto.cpp
@@ -0,0 +1,22 @@
+#include "crypto.h"
+
+// todo: implement this
+
+namespace NVocal {
+
+std::pair<std::string /*privKey*/, std::string /*pubKey*/> GenerateKeys() {
+}
+
+std::string EncryptAsymmetrical(const std::string& pubKey, const std::string& data) {
+}
+
+std::string DecryptAsymmetrical(const std::string& privKey, const std::string& data) {
+}
+
+std::string EncryptSymmetrical(const std::string& key, const std::string& data) {
+}
+
+std::string DecryptSymmetrical(const std::string& key, const std::string& data) {
+}
+
+}
---
+++ projects/vocal/vocal_lib/crypto.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <utility>
+#include <string>
+
+namespace NVocal {
+
+std::pair<std::string /*privKey*/, std::string /*pubKey*/> GenerateKeys();
+std::string EncryptAsymmetrical(const std::string& pubKey, const std::string& data);
+std::string DecryptAsymmetrical(const std::string& privKey, const std::string& data);
+std::string EncryptSymmetrical(const std::string& key, const std::string& data);
+std::string DecryptSymmetrical(const std::string& key, const std::string& data);
+
+}
