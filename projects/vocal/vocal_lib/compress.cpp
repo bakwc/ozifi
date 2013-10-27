4d5a6773b013e50a7b343417f1aab0591a1f213a
Filipp Ozinov <ozinov@quantion.ru>
2013-10-27 12:17:25 +0400

    Compression implemented, closed #11

--- projects/vocal/vocal_lib/compress.cpp
+++ projects/vocal/vocal_lib/compress.cpp
@@ -1,11 +1,24 @@
+#include <utils/exception.h>
+#include <contrib/snappy/snappy.h>
+
 #include "compress.h"
 
 namespace NVocal {
 
 std::string Compress(const std::string& data) {
+    std::string result;
+    if (snappy::Compress(data.data(), data.size(), &result) <= 0) {
+        throw UException("failed to compress data");
+    }
+    return result;
 }
 
 std::string Decompress(const std::string& data) {
+    std::string result;
+    if (!snappy::Uncompress(data.data(), data.size(), &result)) {
+        throw UException("failed to decompress data");
+    }
+    return result;
 }
 
 }
