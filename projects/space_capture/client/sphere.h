cfe7c1086b6e1aca3ad89082db88b7014f7fb4c2
Filipp Ozinov <ozinov@quantion.ru>
2014-02-10 22:56:44 +0400

    Fixed win32 build

--- projects/space_capture/client/sphere.h
+++ projects/space_capture/client/sphere.h
@@ -2,6 +2,13 @@
 
 #include <QtOpenGL/QGLWidget>
 
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+#ifndef M_PI_2
+#define M_PI_2 1.57079632679489661923
+#endif
+
 class SolidSphere
 {
 protected:
