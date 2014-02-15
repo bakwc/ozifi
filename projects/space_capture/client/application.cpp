b797039a538318dab1541ead3f234a5ffbf65ea4
Filipp Ozinov <ozinov@quantion.ru>
2014-02-15 12:27:45 +0400

    Fixed connection

--- projects/space_capture/client/application.cpp
+++ projects/space_capture/client/application.cpp
@@ -25,27 +25,31 @@ void TApplication::Init() {
 }
 
 void TApplication::QuickGame() {
-    disconnect(&MainMenu, &TMainMenu::Render, &Display, &TDisplay::Render);
-    Display.UpdateContol(&Control);
-    Display.UpdateDisplay(&WorldDisplay);
+    try {
+        optional<std::string> address;
+        address = NHttpFetcher::FetchUrl("http://" + std::string(CONTROL_SERVER_ADDRESS) +
+                                         ":" + ToString(CONTROL_SERVER_PORT) + "/quick");
 
-    optional<std::string> address;
-    address = NHttpFetcher::FetchUrl("http://" + std::string(CONTROL_SERVER_ADDRESS) +
-                                     ":" + ToString(CONTROL_SERVER_PORT));
+        if (!address.is_initialized()) {
+            throw UException("Failed to get servers list");
+        }
 
-    if (!address.is_initialized()) {
-        qDebug() << "Failed to get servers list";
-        return;
+        QString hostPortStr = QString::fromStdString(*address);
+        QStringList hostPort = hostPortStr.split(':');
+        if (!hostPort.size() == 2) {
+            throw UException("Failed to get servers list");
+        }
+        ui16 port = FromString(hostPort[1].toStdString());
+
+        disconnect(&MainMenu, &TMainMenu::Render, &Display, &TDisplay::Render);
+        Display.UpdateContol(&Control);
+        Display.UpdateDisplay(&WorldDisplay);
+        Network.ConnectToServer(QHostAddress(hostPort[0]), port);
+    } catch (const std::exception& e) {
+        qDebug() << "Failed to start quick game:" << e.what();
     }
 
-    QString hostPortStr = QString::fromStdString(*address);
-    QStringList hostPort = hostPortStr.split(':');
-    if (!hostPort.size() == 2) {
-        qDebug() << "Wrong address:" << hostPortStr;
-        return;
-    }
 
-    Network.ConnectToServer(QHostAddress(hostPort[0]), FromString(hostPort[1].toStdString()));
 }
 
 void TApplication::Exit() {
